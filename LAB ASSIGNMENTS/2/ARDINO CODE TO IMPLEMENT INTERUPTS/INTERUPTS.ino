1a35e68489f4333dccdff0546263e81b21b1bc57
ANIRUDH KANNAN V P <anirudhkannan.v16@iiits.in>
2018-02-04 14:56:47 +0530

    Add files via upload

---
+++ LAB ASSIGNMENTS/2/ARDINO CODE TO IMPLEMENT INTERUPTS/INTERUPTS.ino
@@ -0,0 +1,25 @@
+int ledOn=0;
+void setup() {
+  
+//put your setup code here, to run once:
+pinMode(8,OUTPUT);
+pinMode(2,INPUT);
+attachInterrupt(0,buttonPressed,RISING);
+}
+
+void loop() {
+  
+  //put your main code here, to run repeatedly:
+}
+
+ void buttonPressed() {
+    if (ledOn)
+    {
+      ledOn=0;
+      digitalWrite(8,LOW);
+    }
+    else {
+      ledOn=1;
+      digitalWrite(8,HIGH);
+    }
+  }
